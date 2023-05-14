f5cf2414165b2393af136106e2732b44b4ab2cfb
cloudswb <zk.life@qq.com>
2023-05-14 20:15:38 +0800

    update

--- main/solution/post-deployment/config/environment-files/init-jupyter-dash.sh
+++ main/solution/post-deployment/config/environment-files/init-jupyter-dash.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Installing ipykernel 6.16.2"
 pip install "ipykernel==6.16.2"
@@ -7,3 +7,5 @@ echo "Installing jupyter-console 6.6.3"
 pip install "jupyter-console==6.6.3"
 
 echo "Finished installed ipykernel and jupyter-console"
+
+exit 0
